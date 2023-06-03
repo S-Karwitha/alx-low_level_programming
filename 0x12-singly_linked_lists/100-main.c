0b06e0c861e87ff3b83174fdd129f8ac804138bb
S-Karwitha <skmbaepersonal@gmail.com>
2023-06-03 07:10:03 -0700

    a function that prints You're beat! and yet, you must allow, I bore my house upon my back! before the main function is executed.

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	printf("(A tortoise, having pretty good sense of a\
-		hare's nature, challenges one to a race.)\n");
+ hare's nature, challenges one to a race.)\n");
 	return (0);
 }
